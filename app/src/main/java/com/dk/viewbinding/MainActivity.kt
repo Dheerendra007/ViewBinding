46ae951a98f5192f21d57531e3a331cb97c5caa3
dkapariya@gmail.com <dkapariya@gmail.com>
2023-10-30 18:37:57 +0530

    ViewBinding with Activity

---
+++ app/src/main/java/com/dk/viewbinding/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.dk.viewbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dk.viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var _viewBinding:ActivityMainBinding? = null
+    private val binding get()= _viewBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        _viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        println( binding.tvName)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _viewBinding = null
+    }
+}
